8637a22478512e91c18090fd4167f6aaee0abd22
abdelbassite badou <abdelbassite.badou@gmail.com>
2021-03-20 01:53:08 +0100

    apollo client setup + QrCode scanne<->restaurant name demo init

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css';
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from '../apollo-client';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    //@ts-ignore
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useBusiness } from '../hooks/useBusiness';
+import dynamic from 'next/dynamic'
+
+const QrReader = dynamic(() => import('react-qr-reader'), { ssr: false })
+
+export default function Home() {
+  let [id, setId] = useState('V7UA2M64');
+  let { loading, data, error } = useBusiness(id);
+  let handleScan = (data: string | null) => {
+    // data && console.log("🚀 ~ file: App.js ~ line 11 ~ handleScan ~ data", data);
+    data && setId(data);
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cloud Menu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div >
+        {loading && <h3>loading...</h3>}
+        {data && data.business && data.business.name && <h1>{data.business.name}</h1>}
+        {/* {data && <h1>{JSON.stringify(data)}</h1>} */}
+        {error && <p>{JSON.stringify(error)}</p>}
+        <QrReader
+          delay={100}
+          style={{
+            height: 240,
+            width: 320,
+          }}
+          onError={console.log}
+          onScan={handleScan}
+        />
+      </div>
+
+    </div>
+  );
+}
