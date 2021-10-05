ebc028adedf3b051e62479a2a6d302459f8625ce
Badou Abdelbassite <a.badou@sanadtech.com>
2021-10-05 20:23:39 +0100

    use the shared libs' parseQRCodeString in the scan handler + support redirecting to external URLs when the scanned QR code contains and non-platform valid URL

--- components/organisms/Scanner.tsx
+++ components/organisms/Scanner.tsx
@@ -1,94 +1,102 @@
-import { useState } from 'react';
-import dynamic from 'next/dynamic'
-import { InstructionsBox, FinePrintCTA } from '@components/atoms'
-import { ScannerDefault, ScannerSuccess, ScannerError } from '../molecules'
-import { parseQrCode } from '@helpers/qrCode'
-import styles from './Scanner.module.sass'
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import { InstructionsBox, FinePrintCTA } from "@components/atoms";
+import { ScannerDefault, ScannerSuccess, ScannerError } from "../molecules";
+import { parseQRCodeString } from "@cloudmenu/cloud-menu-shared-libs";
+import styles from "./Scanner.module.sass";
 
-const QrScanner = dynamic(() => import('react-qr-reader'), { ssr: false })
+const QrScanner = dynamic(() => import("react-qr-reader"), { ssr: false });
+
+const PRIVATE_KEY = "secret";
 
 export interface ScannerProps {
-    onScan: ({ businessId }: { businessId: string }) => void
-    onCancelScan: () => void
+	onScan: ({ businessId }: { businessId: string }) => void;
+	onCancelScan: () => void;
 }
 
 export const Scanner: React.FunctionComponent<ScannerProps> = ({
-    onScan,
-    onCancelScan
+	onScan,
+	onCancelScan,
 }) => {
-    const [scanState, setScanState] = useState<'scan' | 'success' | 'error'>('scan')
+	const [scanState, setScanState] = useState<"scan" | "success" | "error">(
+		"scan"
+	);
 
-    let onErrorHandler = () => {
-        setScanState('error');
-    }
-    let onScanHandler = (data) => {
-        if (!data) return;
-        let { businessId } = parseQrCode(data);
-        if (businessId) {
+	let onErrorHandler = () => {
+		setScanState("error");
+	};
+	let onScanHandler = (data) => {
+		if (!data) return;
+		let { isValidUrl, metaData } = parseQRCodeString(data, PRIVATE_KEY);
+		if (metaData?.businessId) {
+			stopScanner();
+			setScanState("success");
+			onScan({ businessId: metaData?.businessId });
+		} else if (isValidUrl) {
             stopScanner();
-            setScanState('success');
-            onScan({ businessId });
-        } else{
-            stopScanner();
-            setScanState('error');
-        } 
-    }
+            setScanState("success");
+            window.location.href = data;
+		} else {
+			stopScanner();
+			setScanState("error");
+		}
+	};
 
-    let onTryAgainHandler = () => {
-        setScanState('scan');
-    }
+	let onTryAgainHandler = () => {
+		setScanState("scan");
+	};
 
-    let stopScanner = async () => {
-        try {
-            document.querySelector('video').srcObject = null
-            await navigator.mediaDevices
-                .getUserMedia({
-                    video: true,
-                })
-                .then(mediaStream => {
-                    mediaStream.getVideoTracks().forEach(track => {
-                        mediaStream.removeTrack(track);
-                        track.stop();
-                    });
-                });
-            return;
+	let stopScanner = async () => {
+		try {
+			document.querySelector("video").srcObject = null;
+			await navigator.mediaDevices
+				.getUserMedia({
+					video: true,
+				})
+				.then((mediaStream) => {
+					mediaStream.getVideoTracks().forEach((track) => {
+						mediaStream.removeTrack(track);
+						track.stop();
+					});
+				});
+			return;
+		} catch (error) {
+			console.error("stop scanner failed because", error);
+		}
+	};
 
-
-        } catch (error) {
-            console.error('stop scanner failed because', error);
-        }
-
-    }
-
-
-
-    return (
-        <div>
-            {scanState == 'scan' &&
-                <>
-                    <InstructionsBox text="Align the QR code within the frame to scan" />
-                    <div id={styles.scanner}>
-                        <QrScanner
-                        delay={100}
-                        style={{
-                            height: 240,
-                            width: 320,
-                        }}
-                        onError={onErrorHandler}
-                        onScan={onScanHandler}
-                    />
-                    </div>
-                    <FinePrintCTA text="Cancel Scanning" onClick={onCancelScan} />
-                </>
-            }
-            {scanState == 'success' && <ScannerSuccess />}
-            {scanState == 'error' &&
-                <>
-                    <ScannerError />
-                    <FinePrintCTA text="Try Again" onClick={onTryAgainHandler} />
-                </>
-            }
-        </div>
-
-    );
-}
+	return (
+		<div>
+			{scanState == "scan" && (
+				<>
+					<InstructionsBox text="Align the QR code within the frame to scan" />
+					<div id={styles.scanner}>
+						<QrScanner
+							delay={100}
+							style={{
+								height: 240,
+								width: 320,
+							}}
+							onError={onErrorHandler}
+							onScan={onScanHandler}
+						/>
+					</div>
+					<FinePrintCTA
+						text="Cancel Scanning"
+						onClick={onCancelScan}
+					/>
+				</>
+			)}
+			{scanState == "success" && <ScannerSuccess />}
+			{scanState == "error" && (
+				<>
+					<ScannerError />
+					<FinePrintCTA
+						text="Try Again"
+						onClick={onTryAgainHandler}
+					/>
+				</>
+			)}
+		</div>
+	);
+};
